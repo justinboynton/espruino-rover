5ed66a13bbfa99f8f6100fee0551c7012107c175
Justin Boynton <justin.boynton@stylefoundry.co.uk>
2014-07-17 16:17:40 +0100

    Passing basic movement and look tests with Spies

--- spec/RoverSpec.js
+++ spec/RoverSpec.js
@@ -3,26 +3,81 @@ describe("Rover", function() {
 
   beforeEach(function(){
     rover = new Rover();
+    // mock our Espruino functions
+    digitalWrite = jasmine.createSpy();
+    eyes = function() { };
+    eyes.move = jasmine.createSpy();
+    sensor = function() { };
+    sensor.trigger = jasmine.createSpy();
   });
 
   it("should be able to move forwards", function() {
     rover.moveForward();
     expect(rover.currentDirection).toEqual('forward');
+    expect(digitalWrite).toHaveBeenCalled();
   });
 
   it("should be able to move backwards", function() {
     rover.moveBackward();
     expect(rover.currentDirection).toEqual('reverse');
+    expect(digitalWrite).toHaveBeenCalled();
   });
 
   it("should be able to turn left", function() {
     rover.turnLeft();
     expect(rover.currentDirection).toEqual('left');
+    expect(rover.currentMovingStatus).toEqual('stationary');
+    expect(digitalWrite).toHaveBeenCalled();
   });
 
   it("should be able to turn right", function() {
     rover.turnRight();
     expect(rover.currentDirection).toEqual('right');
+    expect(rover.currentMovingStatus).toEqual('stationary');
+    expect(digitalWrite).toHaveBeenCalled();
+  });
+
+  it("should be able to move left", function() {
+    rover.moveLeft();
+    expect(rover.currentDirection).toEqual('left');
+    expect(rover.currentMovingStatus).toEqual('moving');
+    expect(digitalWrite).toHaveBeenCalled();
+  });
+
+  it("should be able to turn right", function() {
+    rover.moveRight();
+    expect(rover.currentDirection).toEqual('right');
+    expect(rover.currentMovingStatus).toEqual('moving');
+    expect(digitalWrite).toHaveBeenCalled();
+  });
+
+  it("should be able to stop", function() {
+    rover.stop();
+    expect(rover.currentMovingStatus).toEqual('stationary');
+    expect(digitalWrite).toHaveBeenCalled();
+  });
+
+  it("should be able to resume travel", function() {
+    rover.resume();
+    expect(rover.currentMovingStatus).toEqual('moving');
+  });
+
+  it("should be able to look left", function() {
+    rover.lookLeft();
+    expect(eyes.move).toHaveBeenCalled();
+    expect(rover.currentLeftDistance).toBeDefined();
+  });
+
+  it("should be able to look right", function() {
+    rover.lookRight();
+    expect(eyes.move).toHaveBeenCalled();
+    expect(rover.currentRightDistance).toBeDefined();
+  });
+
+  it("should be able to look ahead", function() {
+    rover.lookAhead();
+    expect(eyes.move).toHaveBeenCalled();
+    expect(rover.currentForwardDistance).toBeDefined();
   });
 
 });
--- src/Rover.js
+++ src/Rover.js
@@ -1,14 +1,83 @@
 function Rover() {
+  this.currentLeftDistance;
+  this.accumulatedDistance = 0;
+  this.thnkingCount = 1;
 }
 Rover.prototype.moveForward = function() {
+  this.stop();
+  this.currentMovingStatus = "moving";
   this.currentDirection = "forward";
+  digitalWrite("B14", 1);
+  digitalWrite("A1", 1);
 };
 Rover.prototype.moveBackward = function() {
+  this.stop();
+  this.currentMovingStatus = "moving";
   this.currentDirection = "reverse";
+  digitalWrite("B13", 1);
+  digitalWrite("A0", 1);
 };
 Rover.prototype.turnLeft = function() {
+  this.stop();
   this.currentDirection = "left";
+  this.currentMovingStatus = "stationary";
+  digitalWrite("B14", 1);
+  setTimeout(function() { this.stop(); }, 1000);
 };
 Rover.prototype.turnRight = function() {
+  this.stop();
   this.currentDirection  = "right";
+  this.currentMovingStatus = "stationary";
+  digitalWrite("A1", 1);
+  setTimeout(function() { this.stop(); }, 1000);
+};
+Rover.prototype.moveLeft = function() {
+  this.stop();
+  this.currentMovingStatus = "moving";
+  this.currentDirection = "left";
+  digitalWrite("B14", 1);
+};
+Rover.prototype.moveRight = function() {
+  this.stop();
+  this.currentMovingStatus = "moving";
+  this.currentDirection  = "right";
+  digitalWrite("A1", 1);
+};
+Rover.prototype.stop = function() {
+  this.currentMovingStatus = "stationary";
+  digitalWrite("B14", 0);
+  digitalWrite("A1", 0);
+  digitalWrite("B13", 0);
+  digitalWrite("A0", 0);
+};
+Rover.prototype.resume = function() {
+  this.currentMovingStatus = "moving";
+  if(this.currentDirection == "forward")
+    this.moveForward();
+  else if(this.currentDirection == "reverse")
+    this.moveBackward();
+};
+Rover.prototype.lookLeft = function() {
+  this.currentLeftDistance = 0;
+  eyes.move(0);
+  this.watch();
+  setTimeout(function() { this.currentLeftDistance = this.accumulatedDistance / this.thinkingCount; }, 1000);
+};
+Rover.prototype.lookRight = function() {
+  this.currentRightDistance = 0;
+  eyes.move(1);
+  this.watch();
+  setTimeout(function() { this.currentRightDistance = this.accumulatedDistance / this.thinkingCount; }, 1000);
+};
+Rover.prototype.lookAhead = function() {
+  this.currentForwardDistance = 0;
+  eyes.move(1);
+  this.watch();
+  setTimeout(function() { this.currentForwardDistance = this.accumulatedDistance / this.thinkingCount; }, 1000);
+};
+Rover.prototype.watch = function() {
+  this.accumulatedDistance = 0;
+  this.thinkingCount = 1;
+  this.watchInterval  = setInterval(sensor.trigger(), 50);
+  setTimeout(function() { clearInterval(this.watchInterval); }, 1000);
 };
